9db6a3409af4cb8b93a62274f77b7ac79c39caca
tupacsyukur <rifqiaulia987@gmail.com>
2024-12-13 23:19:36 +0700

    add function to calculate one round of rock paper scissor

--- script.js
+++ script.js
@@ -19,3 +19,37 @@ function getHumanChoice() {
         return null;
     }
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    let selectedHuman = humanChoice.toLowerCase();
+
+    if (selectedHuman == 'rock' && computerChoice == 'scissor') {
+        console.log("You win! Rock beats scissor");
+        humanScore++;
+    } else if (selectedHuman == 'scissor' && computerChoice == 'paper') {
+        console.log("You win! Scissor beats paper");
+        humanScore++;
+    } else if (selectedHuman == 'paper' && computerChoice == 'rock') {
+        console.log("You win! Paper beats rock");
+        humanScore++;
+    } else if (selectedHuman == 'scissor' && computerChoice == 'rock') {
+        console.log("You lose! Scissor beats paper");
+        computerScore++;
+    } else if (selectedHuman == 'paper' && computerChoice == 'scissor') {
+        console.log("You lose! Scissor beats paper");
+        computerScore++;
+    } else if (selectedHuman == 'paper' && computerChoice == 'scissor') {
+        console.log("You lose! Scissor beats paper");
+        computerScore++;
+    } else {
+        console.log("It's a tie");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
