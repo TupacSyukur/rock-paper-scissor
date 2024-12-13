85c04c2b6cb25ebb160c8c4afe7b0d5963e215d6
tupacsyukur <rifqiaulia987@gmail.com>
2024-12-13 23:01:14 +0700

    create a computer choice function to get a random rock, paper, or scissor

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+function getComputerChoice() {
+    let num = Math.random();
+    console.log(num);
+    if (num <= 0.33) {
+        return "rock";
+    } else if (num > 0.33 && num <= 0.66) {
+        return "paper";
+    } else {
+        return "scissor";
+    }
+}
